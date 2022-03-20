4fc137f2f181c9bdd3e78fae446f55b7720f230e
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2022-03-20 17:10:18 +0000

    new extention installed.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,29 +1,29 @@
-// import './App.css';
+import './App.css';
 // import Header from './Components/Header/Header';
 // import Nav from './Components/Nav/Nav';
-// import About from './Components/About/About';
+import About from './Components/About/About';
 // import Services from './Components/Services/Services';
 // import Experience from './Components/Experience/Experience';
 // import Footer from './Components/Footer/Footer';
 // import Contacts from './Components/Contact/Contacts';
 // import Portfolio from './Components/Portfolio/Portfolio';
 // import Testimonials from './Components/Testimonial/Testimonials';
-// import React from 'react';
+import React from 'react';
 
-// const App = () => {
-//   return (
-//     <div className="bg">
-//       <Header />
-//       <Nav />
-//       <About />
-//       <Experience />
-//       <Services />
-//       <Portfolio />
-//       <Testimonials />
-//       <Contacts />
-//       <Footer />
-//     </div>
-//   );
-// };
+const App = () => {
+  return (
+    <div className="bg">
+      {/* <Header /> */}
+      {/* <Nav /> */}
+      <About />
+      {/* <Experience /> */}
+      {/* <Services /> */}
+      {/* <Portfolio /> */}
+      {/* <Testimonials /> */}
+      {/* <Contacts /> */}
+      {/* <Footer /> */}
+    </div>
+  );
+};
 
-// export default App;
+export default App;
