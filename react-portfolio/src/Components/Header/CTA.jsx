049e095b41eddffbe5abf2759fbd0adc152ad98b
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2022-03-21 21:08:44 +0000

    One component and a file updated.

--- react-portfolio/src/Components/Header/CTA.jsx
+++ react-portfolio/src/Components/Header/CTA.jsx
@@ -1,13 +1,13 @@
-// import React from 'react'
-// import STWDS from '../../Assets/STWDS.pdf'
+import React from 'react'
+import STWDS from '../../Assets/STWDS.pdf'
 
-// const CTA = () => {
-//   return (
-//       <div className='cta'>
-//           <a href={STWDS} download className='btn'>Download CV</a>
-//           <a href='#contacts' className='btn btn-primary'>Let's Talk</a>
-//     </div>
-//   )
-// }
+const CTA = () => {
+  return (
+      <div className='cta'>
+          <a href={STWDS} download className='btn'>Download my CV</a>
+          <a href='#contacts' className='btn btn-primary'>Let's Talk</a>
+    </div>
+  )
+}
 
-// export default CTA
+export default CTA
