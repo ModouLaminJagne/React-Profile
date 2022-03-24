575039b9d1ed5914feca8b9ec37370ccbd94eb58
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2022-03-24 12:19:14 +0000

    New package installed and form connected to email.

--- react-portfolio/src/Components/Contact/Contacts.jsx
+++ react-portfolio/src/Components/Contact/Contacts.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import './Contacts.css'
 import {MdOutlineEmail} from 'react-icons/md'
-import { AiOutlineLinkedin } from 'react-icons/ai';
+import { AiOutlineWhatsApp } from 'react-icons/ai';
 import { AiOutlineTwitter } from 'react-icons/ai';
+import emailjs from 'emailjs-com'
 
 const Contacts = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_gjm1c5u',
+        'template_3rzzsah',
+        form.current,
+        'azpM4MScq_LOwzZnF'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -14,27 +37,47 @@ const Contacts = () => {
           <article className="contact__option">
             <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>my@email.com</h5>
-            <a href="mailto:myemail@gmail.com">Send a message</a>
-          </article>
-          <article className="contact__option">
-            <AiOutlineLinkedin className="contact__option-icon" />
-            <h4>Twitter</h4>
-            <h5>my@twitter.com</h5>
-            <a href="mailto:myemail@gmail.com">Send a message</a>
+            <h5>jagnemlgh@gmail.com</h5>
+            <a
+              href="mailto:jagnemlgh@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
           </article>
           <article className="contact__option">
             <AiOutlineTwitter className="contact__option-icon" />
-            <h4>Linkedin</h4>
-            <h5>my@linkedin.com</h5>
-            <a href="mailto:myemail@gmail.com">Send a message</a>
+            <h4>Twitter</h4>
+            <a
+              href="https://twitter.com/ModouLJagne"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <AiOutlineWhatsApp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <a
+              href="https://api.whatsapp.com/send?phone=2205368409"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder="Your Name" required />
           <input type="text" name="email" placeholder="Your Email" required />
-          <textarea name="message" rows="7" placeholder="Your Message" required>
-          </textarea>
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
           <button type="submit" className="btn btn-primary">
             Send Message
           </button>
